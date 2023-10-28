e3addde805ce0d47daf80590b5e06e68e90721be
Shadrach Adamu <shadrachadamuul@gmail.com>
2023-10-28 15:54:07 +0100

    aded

--- restart.sh
+++ restart.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 if sudo lsof -ti:7006; then
+  yarn build
   sudo pm2 restart payment-service --watch && sudo pm2 restart payment-service --watch
 else
+  yarn build
   sudo pm2 start process.yml
 fi
